28b2de0f21f8378137f0aa66ab5c1eae049bec36
lukkeswe <johan.lukas@hotmail.com>
2025-06-30 00:53:23 +0000

    Style uploadmanager: Updated the style for the file upload page.

--- www/static/php/filemanager.php
+++ www/static/php/filemanager.php
@@ -16,11 +16,28 @@ if(!isset($_COOKIE["email"]) || !isset($_COOKIE["password"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>File manager</title>
     <script src="static/upmanager.js"></script>
+    <script src="static/docmaanager.js"></script>
 </head>
 <body>
-    <input type="file" id="fileInput" style="border: solid black 1px;">
-    <button onclick="uploadFile()">Upload</button>
+    <header><h1>ファイル管理</h1></header>
+    <div id="container">
+        <main id="main-content">
+            <div id="upload-container">
+                <input type="file" id="fileInput" style="border: solid black 1px;">
+                <br>
+                <button onclick="uploadFile()">Upload</button>
+            </div>
+        </main>
+    </div>
+    
+    <footer><p>&copy;Norlund J. Lukas</p></footer>
+    <script type="text/javasccript">
+        const dm = new DocumentManager();
+        dm.flexContainer();
+    </script>
 </body>
 </html>
