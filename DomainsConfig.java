45bf15d177b9eb68cfd646f1453d7f1e4e632e46
lukkeswe <johan.lukas@hotmail.com>
2025-06-30 05:28:33 +0000

    Domain mapping: Added methods that will assist with the domain mapping for the config file.

--- DomainsConfig.java
+++ DomainsConfig.java
@@ -2,10 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class DomainsConfig {
+    private static final String CONFIG_FILE = "domains.properties";
     public static final Map<String, String> domainMap = new HashMap<>();
-    
+
+    // Load once on startup
     static {
-        try (InputStream input = new FileInputStream("domains.properties")){
+        reload();
+    }
+    // Reload method
+    public static synchronized void reload() {
+        domainMap.clear();
+        try (InputStream input = new FileInputStream(CONFIG_FILE)){
             Properties prop = new Properties();
             prop.load(input);
             for (String key : prop.stringPropertyNames()){
