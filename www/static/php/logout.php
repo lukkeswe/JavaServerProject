2a4b144611528aefe9f033aad817c3bdd21b0338
lukkeswe <johan.lukas@hotmail.com>
2025-07-20 03:34:56 +0000

    Logout simplify

--- www/static/php/logout.php
+++ www/static/php/logout.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
 
-if ($_SESSION["email"] || $_SESSION["password"]){
-    unset($_SESSION["email"]);
-    unset($_SESSION["password"]);
+unset($_SESSION["email"]);
+unset($_SESSION["password"]);
 
-    header("Location:server.php");
-    exit();
-} else {
-    header("Location:server.php");
-    exit();
-}
+header("Location:server.php");
+exit();
+
 ?>
