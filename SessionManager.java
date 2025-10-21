8be51c31725976312e6a36f3544a45085d27358c
lukkeswe <johan.lukas@hotmail.com>
2025-10-21 07:24:28 +0000

    Invalidate session: Made the Session manager invalidate old session of users when they log in.

--- SessionManager.java
+++ SessionManager.java
@@ -4,19 +4,22 @@ import java.util.UUID;
 
 public class SessionManager {
     private static final Map<String, String> sessions = new HashMap<>();
-
+    // Create new session
     public static String createSession(String username) {
+        // Invalidate any exsisting session for this user
+        sessions.entrySet().removeIf(entry -> entry.getValue().equals(username));
+        // Create new session
         String sessionId = UUID.randomUUID().toString();
         sessions.put(sessionId, username);
         System.out.println("Session created:");
         System.out.println(sessions);
         return sessionId;
     }
-
+    // Get username using session id
     public static String getUsername(String SessionId){
         return sessions.get(SessionId);
     }
-
+    // Invalidate session
     public static void invalidateSession(String sessionId){
         sessions.remove(sessionId);
     }
